e2ea761e813371c814c0db7c7c67f622d9d99cb0
Pavel Demin <paveldemin@me.com>
2026-02-25 13:02:29 +0000

    tests: do not use cache in match_versions

--- tests/testthat/test-match_versions.R
+++ tests/testthat/test-match_versions.R
@@ -1,40 +1,11 @@
 test_that("match_versions", {
-  package <- "yadr"
-  version <- "0.0.1"
+  package <- "rlang"
+  version <- "1.0.1"
 
-  description_index <- list(yadr = list("0.0.1" = desc::description$new()))
-  versions_index <- list(
-    yadr = data.frame(
-      package = "yadr",
-      version = "0.0.1",
-      date = "2026-02-19"
-    ),
-    desc = data.frame(
-      package = "desc",
-      version = "0.0.1",
-      date = "2026-02-10"
-    ),
-    rvest = data.frame(
-      package = "rvest",
-      version = "0.0.1",
-      date = "2026-02-10"
-    ),
-    stringr = data.frame(
-      package = "stringr",
-      version = "0.0.1",
-      date = "2026-02-10"
-    ),
-    testthat = data.frame(
-      package = "testthat",
-      version = "0.0.1",
-      date = "2026-02-10"
-    )
-  )
-
-  description <- get_description(package, version, index = description_index)
+  description <- get_description(package, version)
   dependencies <- get_dependencies(description)
-  versions <- get_versions(package, index = versions_index)
+  versions <- get_versions(package)
 
-  result <- match_versions(description, dependencies, index = versions_index)
+  result <- match_versions(description, dependencies)
   testthat::expect_s3_class(result, "data.frame")
 })
